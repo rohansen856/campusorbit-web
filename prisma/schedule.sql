e73387476c2f70912a058a4581d9a4c9b975232d
rohansen856 <rohansen856@gmail.com>
2025-01-11 04:39:53 +0530

    sql updated

--- prisma/schedule.sql
+++ prisma/schedule.sql
@@ -15,3 +15,62 @@ INSERT INTO "public"."Schedule" (id, institute_id, course_code, course_title, pr
 ('iiitdmj_cse_B_4_1_012', 53, 'OE2', 'ELECTIVE', 'N/A', 'theory', 1, '2025-01-01 11:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-01 11:55'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', NULL, 4),
 ('iiitdmj_cse_B_4_1_013', 53, 'CS2008', 'COMPUTER NETWORK', 'ND', 'theory', 1, '2025-01-01 14:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-01 14:55'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
 ('iiitdmj_cse_B_4_1_014', 53, 'CS2007', 'DESIGN AND ANALYSIS OF ALGORITHMS', 'ACP', 'lab', 1, '2025-01-01 15:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-01 17:55'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'CC 3F', 4);
+
+-- CSE Tuesday sem 4
+INSERT INTO "public"."Schedule" (id, institute_id, course_code, course_title, prof, type, day, "from", "to", "group", branch, room, semester) VALUES 
+('iiitdmj_CSE_A_4_2_001', 53, 'CS2008', 'COMPUTER NETWORK', 'ND', 'theory', 2, '2025-01-01 10:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-01 10:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_A_4_2_002', 53, 'CS2009', 'IOT AND EMBEDDED SYSTEM', 'MSg', 'theory', 2, '2025-01-01 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-01 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_A_4_2_003', 53, 'CS2006', 'OPERATING SYSTEM', 'YTD', 'theory', 2, '2025-01-01 14:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-01 14:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_A_4_2_004', 53, 'CS2007L', 'DESIGN AND ANALYSIS OF ALGORITHM', 'ACP', 'lab', 2, '2025-01-01 14:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-01 16:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'CC 3F', 4),
+('iiitdmj_CSE_B_4_2_005', 53, 'CS2006', 'OPERATING SYSTEM', 'YTD', 'theory', 2, '2025-01-02 10:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 10:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_B_4_2_006', 53, 'CS2008', 'COMPUTER NETWORK', 'ND', 'theory', 2, '2025-01-02 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_B_4_2_007', 53, 'CS2009', 'IOT AND EMBEDDED SYSTEM', 'MSg', 'theory', 2, '2025-01-02 14:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 14:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_Z_4_2_008', 53, 'CS2009', 'IOT AND EMBEDDED SYSTEM', 'RKR', 'theory', 2, '2025-01-02 10:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 10:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4),
+('iiitdmj_CSE_Z_4_2_009', 53, 'CS2006', 'OPERATING SYSTEM', 'NA', 'theory', 2, '2025-01-02 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4),
+('iiitdmj_CSE_Z_4_2_010', 53, 'CS2008', 'COMPUTER NETWORK', 'ASP', 'theory', 2, '2025-01-02 14:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 14:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4),
+('iiitdmj_CSE_Z_4_2_011', 53, 'IT2C02', 'NoSQL LAB', 'ADS', 'lab', 2, '2025-01-02 15:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 17:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', NULL, 4),
+('iiitdmj_CSE_A_4_2_011', 53, 'OE2', 'ELECTIVE', 'N/A', 'theory', 2, '2025-01-02 12:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 12:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', NULL, 4),
+('iiitdmj_CSE_B_4_2_012', 53, 'OE2', 'ELECTIVE', 'N/A', 'theory', 2, '2025-01-02 12:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 12:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', NULL, 4),
+('iiitdmj_CSE_Z_4_2_013', 53, 'OE2', 'ELECTIVE', 'N/A', 'theory', 2, '2025-01-02 12:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 12:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', NULL, 4);
+
+-- CSE Wednesday sem 4
+INSERT INTO "public"."Schedule" (id, institute_id, course_code, course_title, prof, type, day, "from", "to", "group", branch, room, semester) VALUES 
+('iiitdmj_CSE_A_4_3_001', 53, 'CS2006', 'OPERATING SYSTEM', 'YDT', 'theory', 3, '2025-01-03 09:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 09:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_A_4_3_002', 53, 'CS2007', 'DESIGN AND ANALYSIS OF ALGORITHMS', 'ACP', 'theory', 3, '2025-01-03 10:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 10:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_A_4_3_003', 53, 'CS2008T', 'COMPUTER NETWORK', 'ND', 'theory', 3, '2025-01-03 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_A_4_3_004', 53, 'CS2009', 'IOT AND EMBEDDED SYSTEM', 'MSg', 'theory', 3, '2025-01-03 12:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 12:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_B_4_3_005', 53, 'CS2007', 'DESIGN AND ANALYSIS OF ALGORITHMS', 'ACP', 'theory', 3, '2025-01-03 09:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 09:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_B_4_3_006', 53, 'CS2008T', 'COMPUTER NETWORK', 'ND', 'theory', 3, '2025-01-03 10:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 10:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_B_4_3_007', 53, 'CS2009', 'IOT AND EMBEDDED SYSTEM', 3, '2025-01-03 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_B_4_3_008', 53, 'CS2006', 'OPERATING SYSTEM', 'YTD', 'theory', 3, '2025-01-03 12:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 12:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_B_4_3_015', 53, 'CS2009L', 'IOT AND EMBEDDED SYSTEM', 'MSg', 'LAB', 3, '2025-01-03 16:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 17:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'CC 2F', 4),
+('iiitdmj_CSE_Z_4_3_009', 53, 'CS2007', 'DESIGN AND ANALYSIS OF ALGORITHM', 'ShM', 'theory', 3, '2025-01-03 09:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 09:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4),
+('iiitdmj_CSE_Z_4_3_010', 53, 'CS2009', 'IOT AND EMBEDDED SYSTEM', 'RKR', 'theory', 3, '2025-01-03 10:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 10:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4),
+('iiitdmj_CSE_Z_4_3_011', 53, 'CS2006', 'OPERATING SYSTEM', 'NA', 'theory', 3, '2025-01-03 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4),
+('iiitdmj_CSE_Z_4_3_012', 53, 'CS2008T', 'COMPUTER NETWORK', 'ASP', 'theory', 3, '2025-01-03 12:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 12:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4),
+('iiitdmj_CSE_Z_4_3_013', 53, 'CS2009L', 'IOT AND EMBEDDED SYSTEM', 'RKR', 'lab', 3, '2025-01-03 14:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 15:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'CC GF', 4),
+('iiitdmj_CSE_Z_4_3_014', 53, 'CS2007L', 'DESIGN AND ANALYSIS OF ALGORITHM', 'ShM', 'lab', 3, '2025-01-03 16:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-03 17:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'CC 3F', 4);
+
+-- CSE Thursday sem 4
+INSERT INTO "public"."Schedule" (id, institute_id, course_code, course_title, prof, type, day, "from", "to", "group", branch, room, semester) VALUES 
+('iiitdmj_CSE_A_4_4_001', 53, 'CS2008', 'COMPUTER NETWORK', 'ND', 'theory', 4, '2025-01-05 09:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-05 09:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_A_4_4_002', 53, 'CS2009', 'IOT AND EMBEDDED SYSTEM', 'MSg', 'theory', 4, '2025-01-05 10:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-05 10:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_A_4_4_003', 53, 'CS2007', 'DESIGN AND ANALYSIS OF ALGORITHMS', 'ACP', 'theory', 4, '2025-01-05 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-05 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_A_4_4_004', 53, 'CS2006', 'OPERATING SYSTEM', 'YTD', 'theory', 4, '2025-01-05 12:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-05 12:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'L104', 4),
+('iiitdmj_CSE_B_4_4_001', 53, 'CS2007', 'DESIGN AND ANALYSIS OF ALGORITHMS', 'ACP', 'theory', 4, '2025-01-06 09:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-06 09:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_B_4_4_002', 53, 'CS2006', 'OPERATING SYSTEM', 'YTD', 'theory', 4, '2025-01-06 10:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-06 10:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_B_4_4_003', 53, 'CS2009', 'IOT AND EMBEDDED SYSTEM', 'MSg', 'theory', 4, '2025-01-06 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-06 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_B_4_4_004', 53, 'CS2008', 'COMPUTER NETWORK', 'ND', 'theory', 4, '2025-01-06 12:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-06 12:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'L207', 4),
+('iiitdmj_CSE_Z_4_4_001', 53, 'CS2006', 'OPERATING SYSTEM', 'NA', 'theory', 4, '2025-01-07 09:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-07 09:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4),
+('iiitdmj_CSE_Z_4_4_002', 53, 'CS2009', 'IOT AND EMBEDDED SYSTEM', 'RKR', 'theory', 4, '2025-01-07 10:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-07 10:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4),
+('iiitdmj_CSE_Z_4_4_003', 53, 'CS2008', 'COMPUTER NETWORK', 'ASP', 'theory', 4, '2025-01-07 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-07 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4),
+('iiitdmj_CSE_Z_4_4_004', 53, 'CS2007', 'DESIGN AND ANALYSIS OF ALGORITHMS', 'ShM', 'theory', 4, '2025-01-07 12:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-07 12:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', 'L105', 4);
+
+
+-- CSE Friday sem 4
+INSERT INTO "public"."Schedule" (id, institute_id, course_code, course_title, prof, type, day, "from", "to", "group", branch, room, semester) VALUES 
+('iiitdmj_CSE_A_4_5_001', 53, 'CS2009L', 'IOT AND EMBEDDED SYSTEM', 'MSg', 'lab', 5, '2025-01-05 09:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-05 10:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', 'CC IOT LAB', 4),
+('iiitdmj_CSE_B_4_5_002', 53, 'IT2C02', 'NoSQL LAB', 'ADS', 'lab', 5, '2025-01-05 14:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-05 16:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', 'CC 1F', 4),
+('iiitdmj_CSE_A_4_5_003', 53, 'OE2', 'ELECTIVE', 'N/A', 'theory', 5, '2025-01-02 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'A', 'CSE', NULL, 4),
+('iiitdmj_CSE_B_4_5_004', 53, 'OE2', 'ELECTIVE', 'N/A', 'theory', 5, '2025-01-02 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'B', 'CSE', NULL, 4),
+('iiitdmj_CSE_Z_4_5_005', 53, 'OE2', 'ELECTIVE', 'N/A', 'theory', 5, '2025-01-02 11:00:00'::timestamp AT TIME ZONE 'Asia/Kolkata', '2025-01-02 11:55:00'::timestamp AT TIME ZONE 'Asia/Kolkata', 'Z', 'CSE', NULL, 4);
